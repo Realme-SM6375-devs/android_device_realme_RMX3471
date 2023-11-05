3cff1368ca895101b85cc5e1d0783c618b53631c
Vivekachooz <vivekachooz@gmail.com>
2023-11-05 08:28:42 +0530

    RMX3471: touch: Expose supported gestures only
    Change-Id: I25e89f5af5e72a1f20d6e77de1afaf36ed089725

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,5 +16,11 @@ ODM_MANIFEST_NFC_FILES := $(DEVICE_PATH)/manifest_nfc.xml
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_RMX3471
 
+# Touch
+SOONG_CONFIG_NAMESPACES += OPLUS_LINEAGE_TOUCH_HAL
+SOONG_CONFIG_OPLUS_LINEAGE_TOUCH_HAL := INCLUDE_DIR
+SOONG_CONFIG_OPLUS_LINEAGE_TOUCH_HAL_INCLUDE_DIR := \
+    $(DEVICE_PATH)/touch/include
+
 # Inherit the proprietary files
 -include vendor/realme/RMX3471/BoardConfigVendor.mk
